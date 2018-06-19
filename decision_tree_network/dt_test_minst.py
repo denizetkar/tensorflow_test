43031b13f4e1d78a482133440b0e8538870b6f6f
denizetkar <denizetkar@gmail.com>
2018-06-20 00:54:13 +0300

    added variational autoencoder test for mnist

--- decision_tree_network/dt_test_minst.py
+++ decision_tree_network/dt_test_minst.py
@@ -1,83 +1,24 @@
 import tensorflow as tf
 import numpy as np
 import os
-import dt_network
-from functools import reduce
-
 import matplotlib.pyplot as plt
 import gc
 
-def int_to_categorical(array, num_of_class):
-    y_categorical = np.zeros((array.shape[0], num_of_class), dtype=np.int32)
-    y_categorical[[i for i in range(array.shape[0])], array] = 1
-    return y_categorical
-
-
-def read_mnist_v2(path, flatten=True):
-    f = np.load(path)
-    x_train, y_train = f['x_train'], f['y_train']
-    x_test, y_test = f['x_test'], f['y_test']
-    f.close()
-    if flatten:
-        x_train = x_train.reshape((x_train.shape[0], -1))
-        x_test = x_test.reshape((x_test.shape[0], -1))
-    else:
-        x_train = x_train.reshape((x_train.shape[0], 28, 28, 1))
-        x_test = x_test.reshape((x_test.shape[0], 28, 28, 1))
-    x_train = x_train.astype('float32')
-    x_test = x_test.astype('float32')
-    x_train /= 255
-    x_test /= 255
-    y_train = int_to_categorical(y_train, 10)
-    y_test = int_to_categorical(y_test, 10)
-    return (x_train, y_train), (x_test, y_test)
-
-
-def get_mnist_dataset_v2(batch_size):
-    # Step 1: Read in data
-    mnist_folder = os.path.join('..', 'data', 'mnist.npz')
-    train, test = read_mnist_v2(mnist_folder, flatten=False)
-
-    # Step 2: Create datasets and iterator
-    train_data = tf.data.Dataset.from_tensor_slices(train)
-    train_data = train_data.batch(batch_size)
-
-    test_data = tf.data.Dataset.from_tensor_slices(test)
-    test_data = test_data.batch(batch_size)
-
-    return train_data, test_data
-
-
-def get_mnist_accuracy(test_dataset, train_output):
-    iterator = tf.data.Iterator.from_structure(test_data.output_types, test_data.output_shapes)
-    initializer = iterator.make_initializer(test_data)
-    inputs, targets = iterator.get_next()
-    inputs = tf.reshape(inputs, [-1, reduce(lambda x, y: x * y, inputs.shape[1:])])
-    targets = tf.reshape(targets, [-1, reduce(lambda x, y: x * y, targets.shape[1:])])
-    with tf.Session() as sess:
-        sess.run(initializer)
-        total_correct_preds = 0
-        batch_num = 0
-        total_size = 0
-        try:
-            while True:
-                batch_target = sess.run(targets)
-                batch_output = train_output[batch_num]
-                correct_pred = np.equal(np.argmax(batch_target, 1), np.argmax(batch_output, 1))
-                batch_accuracy = np.sum(correct_pred.astype(np.float32))
-                total_correct_preds += batch_accuracy
-                batch_num += 1
-                total_size += batch_target.shape[0]
-        except tf.errors.OutOfRangeError:
-            pass
-    return total_correct_preds/total_size
-
+try:
+    import dt_network
+except Exception:
+    from decision_tree_network import dt_network
+try:
+    import utils
+except Exception:
+    from decision_tree_network import utils
+    os.chdir(os.path.join('.', 'decision_tree_network'))
 
 if __name__ == '__main__':
     batch_size = 200
     tf.reset_default_graph()
     with tf.name_scope('data'):
-        train_data, test_data = get_mnist_dataset_v2(batch_size)
+        train_data, test_data = utils.get_mnist_dataset(batch_size)
     dec_tree_net = dt_network.DTNetwork(train_data, num_of_decisions=1,
                                         inference_network=dt_network.CNNSoftmaxClassifier,
                                         decision_network_args={'hidden_layers': []},
@@ -92,7 +33,7 @@ if __name__ == '__main__':
     dec_tree_net.build()
     dec_tree_net.train(epochs=10, save_intervals=1)
     train_output, train_loss = dec_tree_net.eval(test_data)
-    train_acc = get_mnist_accuracy(test_data, train_output)
+    train_acc = utils.get_mnist_accuracy(test_data, train_output)
     print(f'train loss: {train_loss}, train acc: {train_acc}')
 
     del dec_tree_net, train_data, test_data
@@ -110,8 +51,8 @@ if __name__ == '__main__':
     x_test = x_test.astype('float32')
     x_train /= 255
     x_test /= 255
-    y_train = int_to_categorical(y_train, 10)
-    y_test = int_to_categorical(y_test, 10)
+    y_train = utils.int_to_categorical(y_train, 10)
+    y_test = utils.int_to_categorical(y_test, 10)
 
     pred = np.concatenate(train_output, axis=0)
     pred_labels = np.argmax(pred, axis=1)
